9b8632f3920d7c62a69fab32eeee28b6ec98695c
Pham Phu Quoc Bao <39408162+BaoSkyFall@users.noreply.github.com>
2022-03-09 22:52:22 +0700

    change cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sgMail = require('@sendgrid/mail');
 const express = require('express')
 const cors = require('cors')
 const app = express();
-app.use(cors());
+
 sgMail.setApiKey('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');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,27 +12,29 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  app.use(cors({
+    origin: '*'
+  }));
 app.route("/").get(function (req, res) {
    res.send("OK Loaded")
 });
-app.post('/contact',cors(), (req, res) => {
-    console.log(req);
+app.post('/contact', (req, res) => {
+    data = req.body;
     const msg = {
-        to: `sales@dpig-logistics.com`, // Change to your recipient
-        from: 'ops@dpig-logistics.com', // Change to your verified sender
+        to: `sales@dpig-logisticts.com`, // Change to your recipient
+        from: 'ops@dpig-logisticts.com', // Change to your verified sender
         subject: 'Information ',
         template_id:'d-669eb6b3423f45b4948da943bc1edc8c',
         dynamic_template_data:{
-            "name":"Test Test",
-            "email":"sales@dpig-logistics.com",
-            "phoneNumber": "0938443767",
-            "subject":"Toys",
-            "message":"Lorem Ipsum is simply dummy text of the printing and typesetting industry"
-            
+            "name":data.name,
+            "email":data.email,
+            "phoneNumber": data.phoneNumber,
+            "subject":data.subject,
+            "message":data.message
            }
     }
     try {
-        console.log('start send')
+        console.log('start send',msg)
         sgMail.send(msg);
         res.send("Message Successfully Sent!");
       } catch (error) {
